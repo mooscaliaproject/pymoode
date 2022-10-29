aab0ae1f36afa884fde3195d3586977ea51e9f37
mooscalia project <93492480+mooscaliaproject@users.noreply.github.com>
2022-10-29 19:41:44 -0300

    Modified setup and manifest to use pyx files

--- setup.py
+++ setup.py
@@ -1,72 +1,12 @@
+import argparse
+from distutils.errors import CompileError
 import os
 import sys
-from setuptools import find_packages, setup, Extension
-
-import numpy as np
-
-# ---------------------------------------------------------------------------------------------------------
-# ADD MARKED HTML FILES FOR CYTHON
-# ---------------------------------------------------------------------------------------------------------
-
-mark_cython = False
-if "--mark_cython" in sys.argv:
-    mark_cython = True
-    sys.argv.remove("--mark_cython")
-
-if mark_cython:
-    import Cython.Compiler.Options
-    Cython.Compiler.Options.annotate = True
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
 
 # ---------------------------------------------------------------------------------------------------------
-# HANDLING CYTHON FILES
-# ---------------------------------------------------------------------------------------------------------
-
-do_cythonize = False
-if "--cythonize" in sys.argv:
-    do_cythonize = True
-    sys.argv.remove("--cythonize")
-
-ext_modules = []
-cython_folder = os.path.join("pymoode", "cython")
-cython_files = os.listdir(cython_folder)
-
-# if the pyx files should be translated and then compiled
-if do_cythonize:
-    from Cython.Build import cythonize
-    ext_modules = cythonize(["pymoode/cython/*.pyx"])
-
-# otherwise use the existing pyx files - normal case during pip installation
-else:
-    # find all cpp files in czthon folder
-    cpp_files = [f for f in cython_files if f.endswith(".cpp")]
-
-    # add for each file an extension object to be compiled
-    for source in cpp_files:
-        ext = Extension("pymoode.cython.%s" % source[:-4], [os.path.join(cython_folder, source)])
-        ext_modules.append(ext)
-
-if len(ext_modules) == 0:
-    print('*' * 75)
-    print("WARNING: No modules for compilation available.")
-    print("Cython .pyx files are available at the github package repository https://github.com/mooscaliaproject/pymoode")
-    print("To compile .pyx files, run:")
-    print("python setup.py build_ext --inplace --cythonize")
-    print("And then:")
-    print("python setup.py install")
-    print('*' * 75)
-    print("If no compilation occurs, .py files will be used instead, which provide the same results but with worse computational time.")
-    print('*' * 75)
-else:
-    print('*' * 75)
-    print("External cython modules found.")
-    print("To verify compilation success run:")
-    print("from pymoode.survival._metrics import IS_COMPILED")
-    print("This variable will be True to mark compilation success;")
-    print("If no compilation occurs, .py files will be used instead, which provide the same results but with worse computational time.")
-    print('*' * 75)
-    
-# ---------------------------------------------------------------------------------------------------------
-# GENERAL
+# BASE CONFIGURATION
 # ---------------------------------------------------------------------------------------------------------
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -86,7 +26,6 @@ base_kwargs = dict(
     author_email = 'mooscaliaproject@gmail.com',
     url = 'https://github.com/mooscaliaproject/pymoode',
     download_url = 'https://github.com/mooscaliaproject/pymoode',
-    include_dirs = np.get_include(),
     keywords = ['Multi-objective optimization',
                 'GDE3',
                 'NSDE',
@@ -105,13 +44,124 @@ base_kwargs = dict(
         ],
 )
 
+# ---------------------------------------------------------------------------------------------------------
+# ARGPARSER
+# ---------------------------------------------------------------------------------------------------------
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--nopyx', dest='nopyx', action='store_true', help='Whether the pyx files shall be considered at all.')
+parser.add_argument('--nocython', dest='nocython', action='store_true', help='Whether pyx files shall be cythonized.')
+parser.add_argument('--nolibs', dest='nolibs', action='store_true', help='Whether the libraries should be compiled.')
+parser.add_argument('--markcython', dest='markcython', action='store_true', help='Whether to mark the html cython files.')
+
+params, _ = parser.parse_known_args()
+sys.argv = [arg for arg in sys.argv if not arg.removeprefix('--') in params]
+
+# ---------------------------------------------------------------------------------------------------------
+# ADD MARKED HTML FILES FOR CYTHON
+# ---------------------------------------------------------------------------------------------------------
+
+if params.markcython:
+    import Cython.Compiler.Options
+    Cython.Compiler.Options.annotate = True
+
+# ---------------------------------------------------------------------------------------------------------
+# CLASS TO BUILD EXTENSIONS
+# ---------------------------------------------------------------------------------------------------------
+
+# exception that is thrown when the build fails
+class CompilingFailed(Exception):
+    pass
+
+
+# try to compile, if not possible throw exception
+def construct_build_ext(build_ext):
+    class WrappedBuildExt(build_ext):
+        def run(self):
+            try:
+                build_ext.run(self)
+            except BaseException as e:
+                raise CompilingFailed(e)
+
+        def build_extension(self, ext):
+            try:
+                build_ext.build_extension(self, ext)
+            except BaseException as e:
+                raise CompilingFailed(e)
+
+    return WrappedBuildExt
+
+# ---------------------------------------------------------------------------------------------------------
+# HANDLING CYTHON FILES
+# ---------------------------------------------------------------------------------------------------------
+
+ROOT = os.path.dirname(os.path.realpath(__file__))
+
+if params.nopyx:
+    ext_modules = []
+    
+else:
+    try:
+        if params.nocython:
+            path = os.path.join(ROOT, "pymoode", "cython")
+            pyx = [os.path.join(path, f) for f in os.listdir() if f.endswith(".pyx")]
+            ext_modules = [Extension(f"pymoode.cython.{source[:-4]}", [source]) for source in pyx]
+        else:
+            try:
+                from Cython.Build import cythonize
+                ext_modules = cythonize("pymoode/cython/*.pyx")
+            except ImportError:
+                print('*' * 75)
+                print("No Cython package found to convert .pyx files.")
+                print("If no compilation occurs, .py files will be used instead, which provide the same results but with worse computational time.")
+                print('*' * 75)
+                ext_modules = []
+    except:
+        print('*' * 75)
+        print("Problems compiling .pyx files.")
+        print("If no compilation occurs, .py files will be used instead, which provide the same results but with worse computational time.")
+        print('*' * 75)
+        ext_modules = []
+
+if not params.nolibs:
+
+    if len(ext_modules) > 0:
+        base_kwargs['ext_modules'] = ext_modules
+
+        try:
+            import numpy as np
+            base_kwargs['include_dirs'] = [np.get_include()]
+            
+        except BaseException:
+            raise CompileError(
+                "NumPy libraries must be installed for compiled extensions! Speedups are not enabled."
+            )
+        
+        base_kwargs['cmdclass'] = dict(build_ext=construct_build_ext(build_ext))
+    
+    else:
+        print('*' * 75)
+        print("External cython modules found.")
+        print("To verify compilation success run:")
+        print("from pymoode.survival._metrics import IS_COMPILED")
+        print("This variable will be True to mark compilation success;")
+        print("If no compilation occurs, .py files will be used instead, which provide the same results but with worse computational time.")
+        print('*' * 75)
+
+# ---------------------------------------------------------------------------------------------------------
+# RUN SETUP
+# ---------------------------------------------------------------------------------------------------------
+
 compiled_kwargs = base_kwargs.copy()
 compiled_kwargs["ext_modules"] = ext_modules
+
+breakpoint()
 
 try:
     setup(**compiled_kwargs)
     print('*' * 75)
-    print("External cython modules found.")
+    print("Installation successful at the first attempt.")
     print("To verify compilation success run:")
     print("from pymoode.survival._metrics import IS_COMPILED")
     print('*' * 75)
