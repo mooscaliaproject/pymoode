c0015bbd0c8245ca1f033d923b32832575edbf3c
mooscalia project <93492480+mooscaliaproject@users.noreply.github.com>
2022-12-13 23:24:35 -0300

    Include more tests for spacing from book

--- tests/test_spacing.py
+++ tests/test_spacing.py
@@ -1,10 +1,37 @@
 import pytest
 
+import numpy as np
 from pymoo.optimize import minimize
 from pymoo.problems import get_problem
 from pymoode.performance import SpacingIndicator
 from pymoode.algorithms import GDE3
 from pymoode.survival import RankAndCrowding
+
+
+def test_spacing_values():
+    
+    F = np.array([
+        [1.2, 7.8],
+        [2.8, 5.1],
+        [4.0, 2.8],
+        [7.0, 2.2],
+        [8.4, 1.2]
+    ])
+    
+    assert abs(SpacingIndicator().do(F) - 0.73) <= 1e-3
+    
+    F = np.array([
+        [1.0, 7.5],
+        [1.1, 5.5],
+        [2.0, 5.0],
+        [3.0, 4.0],
+        [4.0, 2.8],
+        [5.5, 2.5],
+        [6.8, 2.0],
+        [8.4, 1.2]
+    ])
+
+    assert abs(SpacingIndicator().do(F) - 0.316) <= 1e-3
 
 
 def test_spacing_zero_to_one():
